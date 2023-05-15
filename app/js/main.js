8f40888f47bce8e1fe16f44b06f1af48ca11e779
ftrixy <dmitry.kravetss@gmail.com>
2023-05-15 17:38:16 +0300

    add "footer", start adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,28 @@
-$(function() {
-    
+// ----------- Плавний скрол по кліку на ссилку -----------
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+    anchor.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        const blockID = anchor.getAttribute('href')
+
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+    })
+}
+
+
+// ----------- Меню -----------
+
+let menuBtn = document.querySelector('.header__burger');
+let menu = document.querySelector('.header__menu');
+
+menuBtn.addEventListener('click', ()=> {
+    menu.classList.toggle('header__menu--active');
 });
+
+
